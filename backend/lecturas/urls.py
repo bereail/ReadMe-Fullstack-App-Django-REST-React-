237ef6db47fd106eb955557927cc20edc23663aa
Berenice Solohaga <bsolohaga@hep.local>
2025-12-18 11:53:46 -0300

    url

--- backend/lecturas/urls.py
+++ backend/lecturas/urls.py
@@ -1,11 +1,10 @@
+# lecturas/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-    me, RegisterView,
-    LibroViewSet, LecturaViewSet, AnotacionViewSet,
-    stats_lecturas,
-    buscar_libros, libros_por_subject,
-    guardar_lectura,
+    me, RegisterView, stats_lecturas,
+    guardar_lectura, buscar_libros, libros_por_subject,
+    LibroViewSet, LecturaViewSet, AnotacionViewSet
 )
 
 router = DefaultRouter()
@@ -14,20 +13,17 @@ router.register(r"lecturas", LecturaViewSet, basename="lecturas")
 router.register(r"anotaciones", AnotacionViewSet, basename="anotaciones")
 
 urlpatterns = [
-    # CRUD
     path("", include(router.urls)),
 
-    # USER
+    # user
     path("me/", me),
     path("register/", RegisterView.as_view()),
-
-    # STATS
     path("stats/lecturas/", stats_lecturas),
 
-    # ✅ OPENLIBRARY (público)
+    # openlibrary (público)
     path("openlibrary/buscar/", buscar_libros),
     path("openlibrary/subject/<str:subject>/", libros_por_subject),
 
-    # ✅ GUARDAR (logeado)
+    # guardar (privado)
     path("guardar/", guardar_lectura),
 ]
